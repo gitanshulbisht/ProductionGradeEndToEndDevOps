bbe556889f15e09b56e5cf5bbee83ec5d4684a90
Anshul Bisht <anshul123bisht@gmail.com>
2026-05-26 21:26:49 +0530

    ci: Add GitHub Actions workflow for Terraform and YAML validation

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,8 +47,8 @@ module "eks" {
   # Enable cluster endpoint public access
   cluster_endpoint_public_access = true
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   # EKS Addons
   cluster_addons = {
